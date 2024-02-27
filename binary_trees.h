2318dd38d3b951e1b5bfd4e9a09ac9477953d6f8
ZobaOkafor <judithokafor30@gmail.com>
2024-02-27 06:14:36 -0800

    Header file

--- binary_trees.h
+++ binary_trees.h
@@ -36,11 +36,17 @@ typedef struct binary_tree_s heap_t;
 
 /* Printing function prototype */
 void binary_tree_print(const binary_tree_t *tree);
-static size_t _height(const binary_tree_t *tree);
-static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
+/*static size_t _height(const binary_tree_t *tree);
+static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);*/
 
 
 /* Function prototypes */
+void binary_tree_delete(binary_tree_t *tree);
+int binary_tree_is_leaf(const binary_tree_t *node);
+int binary_tree_is_root(const binary_tree_t *node);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 
 
 #endif
