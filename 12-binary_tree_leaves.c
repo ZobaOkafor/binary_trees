6910a7d355e9e22ceb7741fe98d50c23df2a78f4
ZobaOkafor <judithokafor30@gmail.com>
2024-02-27 09:29:41 -0800

    Count the leaves in a binary tree

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -14,10 +14,5 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
-	/* Recursively count the leaves in the left and right subtrees */
-	size_t left_leaves = binary_tree_leaves(tree->left);
-	size_t right_leaves = binary_tree_leaves(tree->right);
-
-	/* Return the sum of leaves in the left and right subtrees */
-	return (left_leaves + right_leaves);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
