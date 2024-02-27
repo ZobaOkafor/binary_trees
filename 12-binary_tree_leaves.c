a5a3ce8db72a90a3ff21a1eb1d8ca9494b845b43
ZobaOkafor <judithokafor30@gmail.com>
2024-02-27 09:17:06 -0800

    Count the leaves in a binary tree

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,4 +1,4 @@
-nclude "binary_trees.h"
+#include "binary_trees.h"
 
 /**
  * binary_tree_leaves - Counts the leaves in a binary tree
@@ -8,18 +8,16 @@ nclude "binary_trees.h"
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    /* If node is a leaf, return 1 */
-    if (tree->left == NULL && tree->right == NULL)
-        return (1);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
 
-    /* Recursively count the leaves in the left and right subtrees */
-    size_t left_leaves = binary_tree_leaves(tree->left);
-    size_t right_leaves = binary_tree_leaves(tree->right);
+	/* Recursively count the leaves in the left and right subtrees */
+	size_t left_leaves = binary_tree_leaves(tree->left);
+	size_t right_leaves = binary_tree_leaves(tree->right);
 
-    /* Return the sum of leaves in the left and right subtrees */
-    return (left_leaves + right_leaves);
+	/* Return the sum of leaves in the left and right subtrees */
+	return (left_leaves + right_leaves);
 }
-
