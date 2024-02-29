86a7d2ffcddb0c7dd21f9848f997326dd8a3cf5e
ZobaOkafor <judithokafor30@gmail.com>
2024-02-28 18:26:05 -0800

    Check if a binary tree is a valid Binary Search Tree

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,39 +1,39 @@
 #include "binary_trees.h"
 
-
-/**
- * is_bst_util - Checks if a binary tree is a valid binary search tree.
- * @tree: A pointer to the root node of the tree to check.
- * @low: The value of the smallest node visited thus far.
- * @high: The value of the largest node visited this far.
- *
- * Return: If the tree is a valid BST, 1, otherwise, 0.
- */
-int is_bst_util(const binary_tree_t *tree, int low, int high)
-{
-	if (tree != NULL)
-	{
-		if (tree->n < low || tree->n > high)
-			return (0);
-		return (is_bst_util(tree->left, low, tree->n - 1) &&
-				is_bst_util(tree->right, tree->n + 1, high));
-	}
-
-	return (1);
-}
-
-
-
 /**
  * binary_tree_is_bst - Checks if a binary tree is a valid Binary Search Tree.
  * @tree: A pointer to the root node of the tree to check.
  *
- * Return: 1 if tree is a valid BST, and 0 otherwise.
+ * Return: 1 if the tree is a valid BST, 0 otherwise.
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
+	int prev_val = INT_MIN;
+
+	return (is_bst_util(tree, &prev_val));
+}
+
+
+/**
+ * is_bst_util2 - Utility function to check if a tree is a valid BST
+ * using in-order traversal.
+ * @tree: A pointer to the root node of the tree.
+ * @prev_val: A pointer to the previous node's value.
+ *
+ * Return: 1 if the tree is a valid BST, 0 otherwise.
+ */
+int is_bst_util2(const binary_tree_t *tree, int *prev_val)
+{
 	if (tree == NULL)
+		return (1);
+
+	if (!is_bst_util(tree->left, prev_val))
 		return (0);
 
-	return (is_bst_util(tree, INT_MIN, INT_MAX));
+	if (tree->n <= *prev_val)
+		return (0);
+
+	*prev_val = tree->n;
+
+	return (is_bst_util(tree->right, prev_val));
 }
