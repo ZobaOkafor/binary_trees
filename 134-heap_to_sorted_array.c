5d831d9b73ca503cb0929d7363f228c90f99cd73
ZobaOkafor <judithokafor30@gmail.com>
2024-02-29 07:41:30 -0800

    Time complexities

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -1,7 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * heap_to_sorted_array - Converts a Binary Max Heap to a sorted array of integers
+ * heap_to_sorted_array - Converts a Binary Max Heap to
+ * a sorted array of integers
  * @heap: Pointer to the root node of the heap to convert
  * @size: Address to store the size of the array
  *
@@ -9,7 +10,7 @@
  */
 int *heap_to_sorted_array(heap_t *heap, size_t *size)
 {
-	int *sorted_array;
+	int temp, *sorted_array;
 	size_t i = 0, count = 0;
 
 	if (!heap)
@@ -36,10 +37,10 @@ int *heap_to_sorted_array(heap_t *heap, size_t *size)
 	/* Reverse the array to get the sorted array in descending order */
 	for (i = 0; i < count / 2; i++)
 	{
-		int temp = sorted_array[i];
+		temp = sorted_array[i];
 		sorted_array[i] = sorted_array[count - i - 1];
 		sorted_array[count - i - 1] = temp;
 	}
 
-	return sorted_array;
+	return (sorted_array);
 }
